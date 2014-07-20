16f3060bb8842be8dfb981b248ab020671030444
Steven Masuch <smasuch@electricmilk.com>
2014-07-20 11:04:41 -0700

    Added project.

---
+++ PoliceSketch/PoliceSketch/LPSViewController.m
@@ -0,0 +1,24 @@
+//
+//  LPSViewController.m
+//  PoliceSketch
+//
+//  Created by Steven Masuch on 2014-07-20.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import "LPSViewController.h"
+
+@interface LPSViewController ()
+
+@end
+
+@implementation LPSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    // Here is where you will create the buttons & image views and add them to the view.
+}
+
+@end
---
+++ PoliceSketch/PoliceSketchTests/PoliceSketchTests.m
@@ -0,0 +1,34 @@
+//
+//  PoliceSketchTests.m
+//  PoliceSketchTests
+//
+//  Created by Steven Masuch on 2014-07-20.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PoliceSketchTests : XCTestCase
+
+@end
+
+@implementation PoliceSketchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
